c2c4cbe43d802547f79688e66ad164d195c03181
Archi Virani <archivirani01@gmial.com>
2023-08-09 23:05:42 +0530

    common module and return value add

--- src/numbergames/numbergames.controller.ts
+++ src/numbergames/numbergames.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { NumbergamesDetail } from './numbergames.model';
 import { NumbergamesService } from './numbergames.service';
+import { swagger_api_response } from 'src/common/swaggerApiResponse.entity';
 
 
 @Controller('numberwinner')
@@ -8,7 +9,7 @@ export class NumbergamesController {
   constructor(private readonly numbergamesService: NumbergamesService) { }
 
   @Post('numbergames')
-  numbergames(@Body() model: NumbergamesDetail): string {
+  numbergames(@Body() model: NumbergamesDetail): Promise<swagger_api_response> {
     return this.numbergamesService.numbergames(model);
   }
 }
